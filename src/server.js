6572ac3a3bd0643c458f4be4b5fa2daad2471212
thongnd <thongnd@d-soft.com.vn>
2025-09-05 15:56:10 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine.js";
+import initWebRoute from "./route/web.js";
+import connectDB from "./config/connectDB.js";
+
+require('dotenv').config();
+
+let app = express();
+
+// config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoute(app);
+
+connectDB();
+
+let port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
